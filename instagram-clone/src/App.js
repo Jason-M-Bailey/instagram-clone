392f0af3d679a3664ead939a2dc1152bd6a75e0d
Jason Bailey <jmbailey816@gmail.com>
2021-09-18 22:30:22 -0400

    firebase installed

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Post from "./Post.js";
 
-
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "briemferlarson",
+      caption: "hi",
+      imageUrl:
+        "https://pagesix.com/wp-content/uploads/sites/3/2021/09/brie-larson-2.jpg?quality=80&strip=all",
+    },
+    {
+      username: "briemferlarson",
+      caption: "capt mvl",
+      imageUrl:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brie-larson-1543939238.jpg",
+    },
+  ]);
+
   return (
-    <div className="App">
+    <div className="app">
       <div className="app__header">
         <img
           className="app__headerImage"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          src={"https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"}
           alt="Instagram Logo"
         />
       </div>
 
       <h1>insta clone</h1>
-      {/* HEADER */}
 
-      <Post />
-      {/* POST */}
-      {/* POST */}
-      {/* POST */}
+      {
+      posts.map(post => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
       <div className="post__header">
@@ -11,19 +11,18 @@ function Post() {
           alt="username"
           src="/static/images/avatar/1.jpg"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
       {/* header -> avatar + username */}
 
       <img
         class="post__image"
-        src="https://pagesix.com/wp-content/uploads/sites/3/2021/09/brie-larson-1.jpg?quality=90&strip=all&w=721"
+        src={imageUrl}
       ></img>
       {/* image */}
 
       <h4 className="post__text">
-        <strong>brie</strong> hi :)
-      </h4>
+        <strong>{username}</strong>    {caption}</h4>
       {/* username + caption */}
     </div>
   );
